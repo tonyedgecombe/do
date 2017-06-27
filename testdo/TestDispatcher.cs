c161af121d3ca3cb7b1bd0401af6ff519251ad65
Tony Edgecombe <tonyedgecombe@gmail.com>
2017-06-27 10:15:58 +0100

    Some basic tests for Dispatcher class

---
+++ testdo/TestDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Windows.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testdo
+{
+    [TestClass]
+    public class TestDispatcher
+    {
+        [TestMethod]
+        public void TestCurrentDispatcher()
+        {
+            Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+
+            Assert.IsNotNull(dispatcher);
+        }
+
+        [TestMethod]
+        public void TestCurrentIsSameAsThread()
+        {
+            Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+            Dispatcher threadDispatcher = Dispatcher.FromThread(Thread.CurrentThread);
+
+            Assert.AreSame(dispatcher, threadDispatcher);
+        }
+    }
+}
