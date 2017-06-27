a785dd77dc6bff08a5dc630ed040858412b3b18d
Tony Edgecombe <tonyedgecombe@gmail.com>
2017-06-27 10:06:05 +0100

    Add project files.

---
+++ testdo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testdo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testdo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc6c02d5-ea9d-4f9d-8830-880a86ff92dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ testdo/TestDispatcherObject.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Threading;
+
+namespace testdo
+{
+    [TestClass]
+    public class TestDispatcherObject
+    {
+        private class MyDispatcherObject : DispatcherObject
+        {
+        }
+
+        [TestMethod]
+        public void TestCheckAccessOnSameThread()
+        {
+            DispatcherObject d = new MyDispatcherObject();
+            Assert.IsTrue(d.CheckAccess());
+        }
+
+        [TestMethod]
+        public void TestVerifyAccessOnSameThread()
+        {
+            DispatcherObject d = new MyDispatcherObject();
+            d.VerifyAccess();
+        }
+
+        [TestMethod]
+        public void TestCheckAccessOnDifferentThread()
+        {
+            var dispatcherObject = CreateDispatcherObjectOnThread();
+
+            Assert.IsFalse(dispatcherObject.CheckAccess());
+        }
+
+        [TestMethod]
+        public void TestVerifyAccessOnDifferentThread()
+        {
+            var dispatcherObject = CreateDispatcherObjectOnThread();
+
+            Assert.ThrowsException<InvalidOperationException>(() => dispatcherObject.VerifyAccess());
+        }
+
+        [TestMethod]
+        public void TestDispatcherOnSameThread()
+        {
+            var d1 = new MyDispatcherObject();
+            var d2 = new MyDispatcherObject();
+
+            Assert.IsNotNull(d1);
+            Assert.IsNotNull(d2);
+            Assert.AreSame(d1.Dispatcher, d2.Dispatcher);
+        }
+
+        [TestMethod]
+        public void TestDispatcherOnDifferentThreads()
+        {
+            var d1 = CreateDispatcherObjectOnThread();
+            var d2 = CreateDispatcherObjectOnThread();
+
+            Assert.IsNotNull(d1);
+            Assert.IsNotNull(d2);
+            Assert.AreNotSame(d1.Dispatcher, d2.Dispatcher);
+        }
+
+        private static DispatcherObject CreateDispatcherObjectOnThread()
+        {
+            DispatcherObject dispatcherObject = null;
+            var thread = new Thread(() => dispatcherObject = new MyDispatcherObject());
+            thread.Start();
+            thread.Join();
+
+            return dispatcherObject;
+        }
+    }
+}
